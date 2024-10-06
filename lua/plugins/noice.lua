53cd3ee2a07f38c49d56f728afbd94ef5484d11b
Janusz Wozniak <jtwozniak@gmail.com>
2024-10-06 14:24:13 +0200

    ui update, borders etc

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,42 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+      {
+        filter = {
+          event = "notify",
+          -- any = {
+          --   { find = "No information available" },
+          -- },
+        },
+        { skip = true },
+      },
+    },
+    presets = {
+      lsp_doc_border = true,
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+    },
+  },
+}
