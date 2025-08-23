b3fbecb16f24b16c723439af46df6595ccdfb9af
janusz <jtwozniak@gmail.com>
2025-08-24 02:49:10 +0300

    configured oxlint and lint at the same time

--- lua/plugins/lspConfig.lua
+++ lua/plugins/lspConfig.lua
@@ -15,6 +15,26 @@ return {
     --   return orig_util_open_floating_preview(contents, syntax, opts, ...)
     -- end
 
+    opts.servers = {
+      oxlint = {
+        cmd = { "oxc_language_server" },
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          return util.root_pattern(".oxlintrc.json", "package.json")(fname)
+            or util.find_package_json_ancestor(fname)
+            or util.find_node_modules_ancestor(fname)
+            or util.find_git_ancestor(fname)
+        end,
+      },
+    }
     opts.setup = {
       -- bashls = vim.tbl_deep_extend("force", opts.servers.bashls or {}, {
       --   handlers = {
@@ -29,7 +49,7 @@ return {
 
       tailwindcss = function(_, opts)
         local keys = require("lazyvim.plugins.lsp.keymaps").get()
-        keys[#keys + 1] = { "<leader>fl", "<cmd>EslintFixAll<cr>" }
+        keys[#keys + 1] = { "<leader>fl", "<cmd>EslintFixAll<cr><cmd>OxcFixAll<cr>" }
         opts.filetypes = opts.filetypes or {}
 
         -- Additional settings for Phoenix projects
@@ -39,6 +59,22 @@ return {
             classFunctions = { "clsx", "cn", "cva", "twMerge" },
           },
         }
+      end,
+      oxlint = function(_, opts)
+        opts.filtetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        }
+        -- opts.root_dir = function(bufnr, on_dir)
+        --   local fname = vim.api.nvim_buf_get_name(bufnr)
+        --   -- local root_markers = util.insert_package_json({ ".oxlintrc.json" }, "oxlint", fname)
+        --   -- on_dir(vim.fs.dirname(vim.fs.find(root_markers, { path = fname, upward = true })[1]))
+        --   on_dir("~/w/m/.oxlintrs.json")
+        -- end
       end,
     }
   end,
